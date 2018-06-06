84f2789bf5e29f977f405294731d89b9dc19edfc
Richard George <rcgeorge23@gmail.com>
2018-06-06 14:58:28 +0100

    made the page a bit prettier

--- src/main/java/uk/co/novinet/rest/HomeController.java
+++ src/main/java/uk/co/novinet/rest/HomeController.java
@@ -2,10 +2,8 @@ package uk.co.novinet.rest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-import uk.co.novinet.service.member.MemberService;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
