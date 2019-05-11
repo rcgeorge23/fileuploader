6296e6c96600ddd576897329dd3be9547966066f
stratos <stratos-team@bskyb.com>
2019-05-11 07:45:09 +0100

    [RG] fixed compile error

--- src/main/java/uk/co/novinet/rest/StatusController.java
+++ src/main/java/uk/co/novinet/rest/StatusController.java
@@ -5,14 +5,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import uk.co.novinet.service.audit.Audit;
 
 @RestController
 public class StatusController {
 
     @CrossOrigin
     @GetMapping(path = "/status")
-    @Audit
     public ResponseEntity getStatus() {
         return new ResponseEntity(HttpStatus.OK);
     }
